aa490075aa7a6e8db6b915a9bab586395924cbb9
Chris Aasted <caasted@gmail.com>
2016-11-21 18:19:34 -0800

    feat: Add incremental testing

--- routes/stores.py
+++ routes/stores.py
@@ -3,7 +3,12 @@ from routes.common import *
 @app.route('/')
 @app.route('/store/')
 def showStores():
+
+	# Testing section to identify errors
+	store = checkStore(1)
+
 	stores = session.query(Store).order_by(asc(Store.id))
+
 	if 'user_id' not in login_session:
 		state = makeState()
 		return render_template('publicstores.html', stores=stores, state=state)
